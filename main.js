5260a12cd6441ad71d42cec879646a864ec0eb0e
Phelipe Teles <phelipe_teles@hotmail.com>
2025-01-04 17:32:43 -0300

    Add script to build .tex from JSON

---
+++ main.js
@@ -0,0 +1,49 @@
+const nunjucks = require("nunjucks");
+const jsonResume = require("./resume-ptbr.json");
+const fs = require("fs");
+
+nunjucks
+  .configure({ autoescape: false })
+  .addFilter("birthday", birthday)
+  .addFilter("periodDate", periodDate)
+  .addFilter("textit", italic)
+  .addFilter("escapeUnderline", escapeUnderline);
+
+const renderedResume = nunjucks.render("resume-ptbr.njk", jsonResume);
+fs.writeFileSync("resume-ptbr.tex", renderedResume);
+
+function birthday(value) {
+  if (!value) {
+    throw new Error("Unexpected empty date");
+  }
+
+  const [year, month, day] = value.split("-");
+  const date = new Date(year, month - 1, day);
+  return date.toLocaleDateString("pt-BR", {
+    day: "numeric",
+    month: "short",
+    year: "numeric",
+  });
+}
+
+function periodDate(value) {
+  if (!value) {
+    throw new Error("Unexpected empty date");
+  }
+
+  const [year, month, day] = value.split("-");
+  const date = new Date(year, month - 1, day);
+  const s = date.toLocaleDateString("pt-BR", {
+    month: "short",
+    year: "numeric",
+  });
+  return s[0].toUpperCase() + s.slice(1);
+}
+
+function italic(text) {
+  return `\\textit{${text}}`;
+}
+
+function escapeUnderline(text) {
+  return text.replace(/_/g, '\\_')
+}
