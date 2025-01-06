2944b2d0073827bd7219d5f7a98544989808ac2c
Phelipe Teles <phelipe_teles@hotmail.com>
2025-01-06 17:42:10 -0300

    Improve birthday display format

--- scripts/build-resume-html.js
+++ scripts/build-resume-html.js
@@ -4,10 +4,25 @@ const fs = require("fs");
 
 nunjucks
   .configure({ autoescape: true })
+  .addFilter("birthday", birthday)
   .addFilter("formatPeriod", formatPeriod)
 
 const renderedResume = nunjucks.render("src/phelipe-teles-resume-ptbr.html.njk", jsonResume);
 fs.writeFileSync("src/phelipe-teles-resume-ptbr.html", renderedResume);
+
+function birthday(value) {
+  if (!value) {
+    throw new Error("Unexpected empty date");
+  }
+
+  const [year, month, day] = value.split("-");
+  const date = new Date(year, month - 1, day);
+  return date.toLocaleDateString("pt-BR", {
+    day: "numeric",
+    month: "short",
+    year: "numeric",
+  });
+}
 
 function formatPeriod(start, end) {
   if (!start && !end) {
